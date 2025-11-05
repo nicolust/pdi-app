540de0ad4c85775a5c460443e2ee9a7a36cd886d
Sierka02 <n2kasi00@students.oamk.fi>
2025-11-05 12:06:17 +0200

    Movie search test completed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { getAuthentication } from './TMDB/TMDBAuth.jsx'
+import js from '@eslint/js'
 
 async function authtest() {
   const data = await getAuthentication();
@@ -10,27 +11,69 @@ async function authtest() {
 }
 
 function App() {
+
+  const [movies, setMovies] = useState([])
+
+  const apiKey = import.meta.env.VITE_API_KEY_2;
+
+  const fetchData = async (query) => {
+    if (!query) return setMovies([])
+
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`,
+      {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY_2}`,
+          "Content-type": "application/json;charset=utf-8"
+        }
+      }
+    )
+    const movieData = await res.json()
+    setMovies(movieData.results || [])
+  }
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={(authtest)}>
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={(authtest)}>
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+
+        <div>
+          <input onChange={(e) => fetchData(e.target.value)}/>
+
+          <div>{movies.map(movie => (
+            <div key={movie.id}>
+              <img src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                  : "https://via.placeholder.com/150?text=No+Image"
+              } 
+                  alt={movie.title} 
+              />
+              <p>{movie.title}</p>
+            </div>
+          ))}
+          </div>
+        </div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
