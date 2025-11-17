ce1c8a1ab818fb0982d997f9601bf5e3980ee217
nicolust <n3pani00@students.oamk.fi>
2025-11-17 18:33:05 +0200

    mä putoan

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
+import router from "./routes.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
